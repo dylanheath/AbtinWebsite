c8e28f937751db24ac97cf89bc747d3e392f4174
Dylan Heathcote <dylanheathcote14@gmail.com>
2024-12-19 17:45:59 +0000

    more things added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+const path = require("path");
 
 // set the view engine to ejs
 app.set("view engine", "ejs");
@@ -15,6 +16,8 @@ app.get("/", function (req, res) {
 app.get("/about", function (req, res) {
   res.render("pages/about");
 });
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(8080);
 console.log("Server is listening on port 8080");
